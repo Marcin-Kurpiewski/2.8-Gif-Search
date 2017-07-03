9e01c2e5c401747dd8bcca24f067709e233bb618
Marcin-Kurpiewski <kurus.mk@gmail.com>
2017-07-03 10:56:08 +0200

    a async

--- Components/App.js
+++ Components/App.js
@@ -11,34 +11,45 @@ App = React.createClass({
     };
 },
 
-  getGif: function(searchingText, callback) {  // 1.
-    var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;  // 2.
-    var xhr = new XMLHttpRequest();  // 3.
-    xhr.open('GET', url);
-    xhr.onload = function() {
-        if (xhr.status === 200) {
-           var data = JSON.parse(xhr.responseText).data; // 4.
-            var gif = {  // 5.
-                url: data.fixed_width_downsampled_url,
-                sourceUrl: data.url
-            };
-            callback(gif);  // 6.
-        }
-    };
-    xhr.send();
+
+
+  getGif: (searchingText) => {
+	    return new Promise((resolve, reject) => {
+
+              var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;  // 2.
+              var xhr = new XMLHttpRequest();  // 3.
+              xhr.open('GET', url);
+              xhr.onload = function() {
+                  if (xhr.status === 200) {
+                      var data = JSON.parse(xhr.responseText).data; // 4.
+                      var gif = {  // 5.
+                          url: data.fixed_width_downsampled_url,
+                          sourceUrl: data.url
+                      };
+                      resolve(gif);  // 6.
+                  }
+              };
+              xhr.send();
+
+          })
   },
-  
-	handleSearch: function(searchingText) {  // 1.
+
+
+            handleSearch: function(searchingText) {  // 1.
                 this.setState({
                     loading: true  // 2.
                 });
-                this.getGif(searchingText, function(gif) {  // 3.
+
+                this.getGif(searchingText).then((gif)=>{
+
                     this.setState({  // 4
-                      loading: false,  // a
-                      gif: gif,  // b
-                      searchingText: searchingText  // c
-                  });
-              }.bind(this));
+                        loading: false,  // a
+                        gif: gif,  // b
+                        searchingText: searchingText  // c
+                    });
+
+                })
+
             },
 
 	render: function(){
